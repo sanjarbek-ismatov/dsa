a7e052cee4fd7d8735e83d529df94e08805478a5
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-01 18:50:37 +0500

    stack: stack with vector

--- Data Structures/Stack/C, C++/main.cpp
+++ Data Structures/Stack/C, C++/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
-#include <stack>
+//#include <stack>
+#include <vector>
 class Stack{
-
+public:
+    void push(int data){
+        stack.push_back(data);
+    }
+    int pop(){
+        int last = stack.back();
+        stack.pop_back();
+        return last;
+    }
+    int top(){
+        return stack.front();
+    }
+    int size(){
+        return (int)stack.size();
+    }
+private:
+    std::vector<int> stack;
+    bool is_empty(){
+        return stack.empty();
+    }
 };
 int main(){
-    std::stack<int> stack;
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    std::cout << stack.top() << std::endl;
+    auto stack = new Stack();
+//    std::stack<int> stack;
+    stack -> push(1);
+    stack -> push(2);
+    stack -> push(3);
+    std::cout << stack -> size() << std::endl;
     return 0;
 }
