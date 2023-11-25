f0ec6f52f8abe6ae044dd1f2244846cc758f7489
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-11-25 11:49:52 +0500

    Find the Symmetric Difference: javascript

---
+++ FreeCodeCamp course/Algorithms/Find the Symmetric Difference/index.js
@@ -0,0 +1,15 @@
+function sym(...args) {
+    function symDifference(arr1, arr2){
+        const result = []
+        for(const item1 of arr1){
+            if(!arr2.includes(item1) && !result.includes(item1)) result.push(item1)
+        }
+        for(const item2 of arr2){
+            if(!arr1.includes(item2) && !result.includes(item2)) result.push(item2)
+        }
+        return result
+    }
+    return args.reduce(symDifference);
+}
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
