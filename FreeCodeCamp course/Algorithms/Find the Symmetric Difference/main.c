f087504ffc63566af284a79deb493af930dbadf4
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-11-28 09:38:36 +0500

    gitignore fixes && task is not working for now

--- FreeCodeCamp course/Algorithms/Find the Symmetric Difference/main.c
+++ FreeCodeCamp course/Algorithms/Find the Symmetric Difference/main.c
@@ -4,14 +4,14 @@
 #include <stdarg.h>
 #include <string.h>
 bool includes(const int*arr, int item){
-    const size_t length = sizeof(*arr) / sizeof(int);
+    const int length = sizeof(*arr) / sizeof(int);
     for(int i = 0; i < length; i++){
         if(arr[i] == item) return true;
     }
     return false;
 }
-void iterator(int* index, const int* arr1, const int * arr2, int* result){
-    const size_t length = sizeof(*arr1) / sizeof(int);
+void iterator(int* index, const int* arr1,  int * arr2, int* result){
+    const int length = sizeof(*arr1) / sizeof(int);
     for(int i = 0; i < length; i++){
         int current = arr1[i];
         if(!(includes(arr2, current) && includes(result, current))){
@@ -30,23 +30,24 @@ void print(int length,const int* array){
     strcpy(result_string, "]");
     printf("%s", result_string);
 }
-int* sym_difference(const int * arr1, const int * arr2){
+int* sym_difference(int * arr1, int * arr2){
     int* result = (int*)malloc(10 * sizeof(int));
     int index = 0;
     iterator(&index, arr1, arr2, result);
     iterator(&index, arr2, arr1, result);
     return result;
 }
-int* sym(int length, int * arrays, ...){
-    int* result = malloc(length);
+int* sym(int length, int count, ...){
+    int* result = (int*)malloc(length * sizeof(int));
     va_list args;
-    va_start(args, arrays);
-    for(int i = 0;  i < arrays; ++i){
-        int* array = va_arg(args, int*);
-        if(length == 0){
-            result = array;
+    va_start(args, count);
+    for(int i = 0;  i < count; i++){
+        int* array = va_arg(args, int *);
+        if(i == 0){
+         result = array;
+        }else {
+            result = sym_difference(result, array);
         }
-        result = sym_difference(result, array);
     }
     va_end(args);
     return result;
@@ -55,7 +56,7 @@ int main(){
     int array1[] = {1, 2, 3};
     int array2[] = {5, 2, 1, 4};
     int length = (sizeof(array1) + sizeof(array2)) / sizeof(int);
-    int* result = sym(length,array1,array2);
+    int* result = sym(length, 2, array1,array2);
     print(length, result);
     free(result);
     return 0;
