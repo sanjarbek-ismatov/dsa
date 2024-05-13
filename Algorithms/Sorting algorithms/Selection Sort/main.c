aeb008d267d728eebc05be88e38ae2c7f9e5fd38
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-05-13 12:15:05 +0500

    optimizations

--- Algorithms/Sorting algorithms/Selection Sort/main.c
+++ Algorithms/Sorting algorithms/Selection Sort/main.c
@@ -2,7 +2,7 @@
 #include"../../../Libraries/utils.h"
 int* selection_sort(int given_arr[], int length);
 int main(){
-    int unsorted_arr[] = {2, 4, 1, 5, 3};
+    int unsorted_arr[] = {2, 4, 1, 5, 3, 8, 2, 9, 0, 4};
     int length_of_arr = length(sizeof(unsorted_arr));
     int* result = selection_sort(unsorted_arr, length_of_arr); 
     print_arr(result, length_of_arr);
@@ -10,10 +10,10 @@ int main(){
 }
 int* selection_sort(int given_arr[], int length){
     int* arr = given_arr;
+    int curr = arr[0];
     for(int i = 0; i < length; i++){
         int min = find_min(given_arr, length, i, length);
-        printf("%d %d\n", arr[i], arr[min]);
-        int curr = arr[i];
+        curr = arr[i];
         arr[i] = arr[min];
         arr[min] = curr;
     }
