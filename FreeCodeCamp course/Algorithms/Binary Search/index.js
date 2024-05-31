d11056f260ba5a78985962fc7e4bc520e59e49eb
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-05-31 21:41:20 +0500

    it's been ages since I have written code in Javascript

--- FreeCodeCamp course/Algorithms/Binary Search/index.js
+++ FreeCodeCamp course/Algorithms/Binary Search/index.js
@@ -1,17 +1,32 @@
+/**
+ * 
+ * @param {number[]} searchList 
+ * @param {number} value 
+ * @returns 
+ */
 function binarySearch(searchList, value) {
+  // checked arrays
   let arrayPath = [];
+  // left, right or low and high
   let left = 0,
     right = searchList.length - 1,
+    // if it is found, it gets true
     found = false;
+  // if there is no target and left is already bigger than right, the loop stops
   while (left <= right) {
+    // middle to compare with the target
     let mid = Math.floor((left + right) / 2);
+    // to record how many steps done
     arrayPath.push(searchList[mid]);
     if (searchList[mid] === value) {
+      // the loop breaks here
       found = true;
       break;
+      // deciding what to do with the array
     } else if (searchList[mid] > value) right = mid - 1;
     else left = mid + 1;
   }
+  // finally, the result is returned
   return found ? arrayPath : "Value Not Found";
 }
 
