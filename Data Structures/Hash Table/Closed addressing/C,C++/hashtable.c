34431f9d9cbdf208299d58dec1520d97ef64b257
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-07-15 23:09:31 +0500

    Basic hash table implementation in C

--- Data Structures/Hash Table/Closed addressing/C,C++/hashtable.c
+++ Data Structures/Hash Table/Closed addressing/C,C++/hashtable.c
@@ -1,7 +1,31 @@
+#include<stdio.h>
 #include "../../../Linked List/C,C++/linkedlist.h"
+int hash(char* string, int size_of_table){
+    int i = 0;
+    int hashed = 0;
+    while(string[i] != '\0'){
+        hashed += (int)string[i++];
+    }
+    return hashed % size_of_table;
+}
+char** hash_table_init(int size){
+    char** array = (char**)malloc(256 * size);
+    return array;
+}
+void insert(char* item, char** table, int table_size){
+    int hashed = hash(item, table_size);
+    // Conditionals should be written
+      printf("%s\n", table[hashed]);
+    table[hashed] = item;
+  
+}
 int main(){
-    LinkedList list;
-    prepend(&list, 1);
-    display(&list);
+    // LinkedList list;
+    // constructor(&list);
+    // prepend(&list, 1);
+    // prepend(&list, 2);
+    // display(&list);
+    char** table = hash_table_init(10);
+    insert("Sanjarbek", table, 10);
     return 0;
 }
